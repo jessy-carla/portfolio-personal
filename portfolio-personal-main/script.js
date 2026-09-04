53b737298e8b6ffdf464c100ee203937e8254bc5
ItaloSA <aluno.cadsa@SISTEMAFIEPE.LOCAL>
2026-09-04 10:30:32 -0300

    Correção e adição de arquivos do portfolio

---
+++ portfolio-personal-main/script.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementById("menuButton");
+
+const mobileMenu = document.getElementById("mobileMenu");
+
+
+menuButton.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("hidden");
+
+});
